dcdc9520bae2b4898904da31abeab0a5139d3c39
raya <raya@rhp-valuation.com>
2022-03-31 23:03:30 +0700

    fix mobile viewport

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,5 +17,17 @@ export default {
       class: "font-poppins text-gray-500",
     },
   },
+  mounted() {
+    this.setViewHeight();
+    window.addEventListener("resize", () => {
+      this.setViewHeight();
+    });
+  },
+  methods: {
+    setViewHeight: function () {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    },
+  },
 };
 </script>
