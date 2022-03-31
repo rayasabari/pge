993cfcafbfe74682554b7dc432f17f5648ad5fbc
raya <raya@rhp-valuation.com>
2022-03-31 14:27:02 +0700

    change target

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -31,7 +31,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: './plugins/vue-agile.js'},
+    { src: './plugins/vue-agile.js' },
   ],
 
 
