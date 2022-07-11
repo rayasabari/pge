f94781d75ee066d3a0f7042b7197da2a83772f47
raya <raya@rhp-valuation.com>
2022-07-11 13:12:39 +0700

    footer bg

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bottom-0 w-full bg-primary-700 text-primary-100">
+  <div class="bottom-0 w-full text-gray-300 bg-gray-800">
     <Container>
       <div
         class="flex items-center justify-center text-xs py-7 h-14"
-      >&copy; 2022 All Rights Reserved</div>
+      >&copy; 2022 Prodigy Green Energy</div>
     </Container>
   </div>
 </template>
